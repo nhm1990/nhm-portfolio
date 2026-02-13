f546062af5d603d1163506a6b5ede9e891df4214
nhm1990 <nhm1990@users.noreply.github.com>
2026-02-13 12:27:46 +0100

    Init nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@primevue/nuxt-module',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
